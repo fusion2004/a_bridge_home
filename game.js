dd100b25ba0f9e99436cbec077a415f886e81427
fusion2004 <fusion2004@gmail.com>
2012-02-24 18:40:52 -0500

    added logo and shooting sound

--- game.js
+++ game.js
@@ -7,13 +7,24 @@ window.onload = function() {
   Crafty.background("#000");
   
   Crafty.sprite(32, "assets/shipwip.png", { shipsprite: [0, 0]});
-  
-  var player = Crafty.e("2D, Canvas, Fourway, shipsprite")
-                     .attr({x: 160, y: 96, w: 32, h: 32})
-                     .fourway(5);
+  Crafty.sprite("assets/titlewip.png", { logosprite: [0, 0, 100, 40]});
   
   Crafty.audio.add("main_theme", "assets/music/fusion2004_-_a_bridge_home_-_theme_wip1.mp3");
+  Crafty.audio.add("shoot", "assets/sound/shoot.wav");
+  Crafty.audio.settings("shoot", { volume: 0.2 });
   Crafty.audio.play("main_theme", -1);
+  
+  var player = Crafty.e("2D, Canvas, Fourway, Keyboard, shipsprite")
+                     .attr({x: 160, y: 96, w: 64, h: 64})
+                     .fourway(5)
+                     .bind("KeyDown", function(e) {
+                       if(e.keyCode === Crafty.keys.SPACE) {
+                         Crafty.audio.play("shoot");
+                       }
+                     });
+  var logo = Crafty.e("2D, Canvas, logosprite")
+                   .attr({x: 20, y: 0, w: 200, h: 80});
+  
   
   //var pl = Crafty.e("2D, Canvas, Color, Fourway")
   //                .attr({x: 160, y: 96, w: 32, h: 32}) // for Component 2D
